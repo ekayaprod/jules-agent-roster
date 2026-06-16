544b0c6af4fe8deccde24e9da8403f4c3fc537b5
ekayaprod <errolkaya@outlook.com>
2026-06-15 22:16:13 -0400

    Delete update_tasks.py

--- update_tasks.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open(".jules/agent_tasks.md", "r") as f:
-    text = f.read()
-
-# Unfortunately it's the UNKNOT queue we were looking at, but it wasn't listed there explicitly. Let's make sure we update it properly anyway if there was one, or if not, just leave it.
-
-print("done")
